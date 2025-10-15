de33fb11253878746a9a1c2169ac881453dce030
Mehmet Artun <mehmet@artun.com>
2025-10-14 21:49:36 -0700

    repo

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:csen268/pages/login/views/email_password_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../repositories/authentication/authentication_repository.dart';
 import 'cubit/login_cubit.dart';
 import 'views/error_view.dart';
 import 'views/success_view.dart';
@@ -12,7 +13,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => LoginCubit(),
+      create: (context) => LoginCubit(RepositoryProvider.of<AuthenticationRepository>(context)),
       child: BlocBuilder<LoginCubit, LoginState>(
         builder: (context, state) {
           switch (state) {
--- toc.md
+++ toc.md
@@ -6,4 +6,6 @@ The content in this repo follows the CSEN268 Spring25 Lecture at Santa Clara Uni
 - [Lecture 6 - 01](https://github.com/mehmetartun/CSEN268/tree/L06.01) Mock Service, User Class, etc.
 - [Lecture 6 - 02](https://github.com/mehmetartun/CSEN268/tree/L06.02) Intro into Bloc/Cubit
 - [Lecture 7 - 01](https://github.com/mehmetartun/CSEN268/tree/L07.01) 
-Implementation of Bloc and Repository
+Implementation of Form to input email and password
+- [Lecture 7 - 02](https://github.com/mehmetartun/CSEN268/tree/L07.02) 
+Implementation of Repository
